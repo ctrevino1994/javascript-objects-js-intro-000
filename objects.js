7720f2024ad67b48e80dd5d403e8ac1b97093a1d
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-19 00:13:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist =  new Object({artist: 'song'})
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { ['Phil Ochs']: "Here's to the state of Mississippi" })
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName;
+}
